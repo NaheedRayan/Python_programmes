97751c889b7767689b428bdd79b966285641db66
NaheedRayan <58568817+NaheedRayan@users.noreply.github.com>
2020-09-27 00:21:31 +0600

    fs

--- ScreenSaver/sudoko_version2.py
+++ ScreenSaver/sudoko_version2.py
@@ -49,17 +49,17 @@ def isValid(grid, i, j, e):
        
 
 def solveSudoku(grid, i=0, j=0):
-        i,j = findNextCellToFill(grid, i, j)
-        if i == -1:
-            return True
-        for e in range(1,10):
-            if isValid(grid,i,j,e):
-                grid[i][j] = e
-                if solveSudoku(grid, i, j):
-                    return True
-                # Undo the current cell for backtracking
-                grid[i][j] = 0
-        return False
+    i,j = findNextCellToFill(grid, i, j)
+    if i == -1:
+        return True
+    for e in range(1,10):
+        if isValid(grid,i,j,e):
+            grid[i][j] = e
+            if solveSudoku(grid, i, j):
+                return True
+            # Undo the current cell for backtracking
+            grid[i][j] = 0
+    return False
 
 
 
